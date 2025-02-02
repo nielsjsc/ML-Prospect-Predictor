8726884f61bf1c8a2e53bef90c6792e52b30f99c
Niels Christoffersen <70546911+nielsjsc@users.noreply.github.com>
2025-02-01 16:27:13 -0800

    implemented constants
    grades for prospects and teams (organizations)

---
+++ frontend/src/constants/grades.tsx
@@ -0,0 +1,14 @@
+export const HITTER_GRADES = [
+  { field: 'Hit_future', label: 'Hit' },
+  { field: 'Game_future', label: 'Game Power' },
+  { field: 'Raw_future', label: 'Raw Power' },
+  { field: 'Spd_future', label: 'Speed' }
+];
+
+export const PITCHER_GRADES = [
+  { field: 'FB_future', label: 'Fastball' },
+  { field: 'SL_future', label: 'Slider' },
+  { field: 'CB_future', label: 'Curveball' },
+  { field: 'CH_future', label: 'Changeup' },
+  { field: 'CMD_future', label: 'Command' }
+];
